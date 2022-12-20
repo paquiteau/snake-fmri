aa86ad593166d1b957234dcc7ff1535a296438d6
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-20 18:49:04 +0100

    extend the base class to specify noise adder handlers.

--- src/simfmri/simulator/handlers/noise.py
+++ src/simfmri/simulator/handlers/noise.py
@@ -1,23 +1,55 @@
 from .base import AbstractHandler
 from ..simulation import Simulation
 
+from simfmri.utils import validate_rng, RngType
+
+import numpy as np
+import scipy.stats as sps
+
 
 class NoiseHandler(AbstractHandler):
-    """Add noise to the data"""
+    """Add noise to the data
 
-    def __init__(self, mu, sigma):
-        self.mu = mu
-        self.sigma = sigma
+    Parameters
+    ----------
+    snr: The target SNR
+    rng: int or numpy random state
+    verbose: verbose flag.
+        If True, a callback function is setup to return the computed snr.
+
+    """
+
+    def __init__(self, verbose=True, rng: RngType = None, snr: float = 0):
+        super().__init__()
+        self._verbose = verbose
+        self._rng = validate_rng(rng)
+        if self._verbose:
+            self.add_callback(self._callback_fun)
+
+    def _callback_fun(self, old_sim, new_sim):
+        if self._verbose:
+            # compute the SNR and print it.
+            print("actual_snr:")
 
     def _handle(self, sim: Simulation):
-        pass
+        if self._snr == 0:
+            return sim
+        else:
+            self._add_noise(sim)
+
+    def _add_noise(self, sim):
+        raise NotImplementedError
 
 
-class GaussianNoiseHandler(AbstractHandler):
-    def _handle(self, sim: Simulation):
-        pass
+class GaussianNoiseHandler(NoiseHandler):
+    """Add gaussian Noise to the data."""
+
+    def __init__(self, snr=0, rng=None):
+        self._snr = snr
 
 
-class RicianNoiseHandler(AbstractHandler):
-    def _handle(self, sim: Simulation):
-        pass
+class RicianNoiseHandler(NoiseHandler):
+    """
+    Add rician noise to the data.
+
+    """
