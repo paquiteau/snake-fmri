7f810658df4af5f7600bb9bcc9706a07e96dd2eb
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-20 18:49:52 +0100

    finish phantom data creation.

--- src/simfmri/simulator/handlers/phantom.py
+++ src/simfmri/simulator/handlers/phantom.py
@@ -14,11 +14,18 @@ class SheppLoganPhantomGeneratorHandler(AbstractHandler):
 
     Parameters
     ----------
-    B0: main magnetic field intensity.
+    B0:
+        main magnetic field intensity.
+    roi_index:
+        the index for the region of interest.
+        It is one of the ellipsis defined in the phantom.
 
+    See Also
+    --------
+    utils.phantom: Module defining the shepp logan phantom.
     """
 
-    def __init__(self, B0: Union(int, float) = 7, roi_index=10):
+    def __init__(self, B0: Union(int, float) = 7, roi_index: int = 10):
         self.B0 = B0
         self.roi_index = roi_index
 
@@ -29,4 +36,8 @@ class SheppLoganPhantomGeneratorHandler(AbstractHandler):
         M0, T1, T2, labels = mr_shepp_logan(sim.shape, B0=self.B0, T2star=True)
 
         sim.data_ref = T2
+        sim.data = T2.copy()
+
         sim.roi = labels == self.roi_index
+
+        return sim
