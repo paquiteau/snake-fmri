999917b6e6d68acca3a1c91b5d563ca3d95926b6
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-21 19:03:58 +0100

    add integrity function for simulation data.

--- src/simfmri/simulator/simulation.py
+++ src/simfmri/simulator/simulation.py
@@ -55,5 +55,26 @@ class Simulation:
         return copy.deepcopy(self)
 
     @property
-    def duration(self):
+    def duration(self) -> float:
         return self.TR * self.n_frames
+
+    def is_valid(self) -> bool:
+        """ "Check if the attributes are coherent to each other."""
+        if self.data_ref:
+            if self.data_ref.shape != (self.n_frames, *self.shape):
+                return False
+            if self.data_acq.shape != self.data_ref.shape:
+                return False
+
+        if self.smaps:
+            if self.smaps.shape != (self.n_coils, *self.shape):
+                return False
+        if self.kspace_data:
+            if self.kspace_data.shape[0] != self.n_frames:
+                return False
+        if self.kspace_data and self.smaps:
+            if self.kspace_data.shape[1] != self.n_coils:
+                return False
+        # TODO: add test on the mask
+
+        return True
