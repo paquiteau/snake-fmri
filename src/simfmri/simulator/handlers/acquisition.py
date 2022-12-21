9c6d07fe8f12908808e36b21bc6a867c657d5ff5
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-21 19:42:54 +0100

    complete acquisition module.

--- src/simfmri/simulator/handlers/acquisition.py
+++ src/simfmri/simulator/handlers/acquisition.py
@@ -1,25 +1,35 @@
-from .base import AbstractHandler
-from fmri.operator.fourier import CartesianSpaceFourier
-from typing import Union
+"""Acquisition Handler to genereate Kspace data from simulation."""
+from typing import Callable, Union
+
 import numpy as np
+from fmri.operator.fourier import CartesianSpaceFourier
 from simfmri.utils import get_smaps
+from simfmri.utils.cartesian_sampling import get_cartesian_mask
+
+from ..simulation import Simulation
+from .base import AbstractHandler
 
 
 class AcquistionHandler(AbstractHandler):
-    """Simulate the acquisition of the data.
+    """
+    Simulate the acquisition of the data.
 
     Parameters
     ----------
+    sampling_mask: ndarray or callable
+    gen_smaps: boolean
 
     """
 
-    def __init__(self, sampling_mask: np.ndarray, gen_smaps=True):
+    def __init__(
+        self, sampling_mask: Union(np.ndarray, Callable), gen_smaps: bool = True
+    ):
         self._sampling_mask = sampling_mask
         self._gen_smaps = gen_smaps
 
         pass
 
-    def _handle(self, sim):
+    def _handle(self, sim: Simulation):
         if self._gen_smaps:
             sim.smaps = get_smaps(sim.shape, sim.n_coils)
 
@@ -41,17 +51,32 @@ class AcquistionHandler(AbstractHandler):
         pass
 
     @classmethod
-    def const_vds(cls, acs: Union(float, int)):
-
-        pass
-
-    @classmethod
-    def variable_vds(
+    def vds(
         cls,
         acs: Union(float, int),
+        accel: int,
+        constant: bool = True,
+        gen_smaps: bool = True,
     ):
-        pass
+        """
+        Generate Acquisition with Variable density sampling (vds).
 
-    @classmethod
-    def const_us(cls, acs: int, R: int = 2):
-        pass
+        Parameters
+        ----------
+        acs: autocalibration line number (int) or proportion (float)
+        accel: Acceleration factor
+        constant: If true, the sampling pattern is repeated across time
+        gen_smaps: If true, smaps are generated, and used in the acquisition.
+        """
+
+        def sampling_mask(sim: Simulation):
+            return get_cartesian_mask(
+                shape=sim.shape,
+                n_frames=sim.n_frames,
+                constant=constant,
+                center_prop=acs,
+                accel=accel,
+                pdf="gaussian",
+            )
+
+        return cls(sampling_mask=sampling_mask, gen_smaps=gen_smaps)
