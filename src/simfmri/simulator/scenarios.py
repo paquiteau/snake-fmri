5b9221c4f904517060079bf9cb51bd3b8c940fe9
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-30 17:27:20 +0100

    first simple scenario

---
+++ src/simfmri/simulator/scenarios.py
@@ -0,0 +1,47 @@
+"""Example of scenarios for the simulator."""
+
+from simfmri.utils import Shape2d3d
+
+from .simulation import Simulation
+from .handlers import (
+    SheppLoganPhantomGeneratorHandler,
+    ActivationHandler,
+    AcquisitionHandler,
+)
+
+
+def sl_block_vds(n_frames: int, shape: Shape2d3d, snr: float, R: int) -> Simulation:
+    """Generate a simulation scenario using shepp-logan and block activation.
+
+    The TR is fixed to 1s.
+
+    Parameters
+    ----------
+    n_frames
+        number of frames
+    shape
+        Shape of the simulated volume
+    snr
+        Signal to noise ratio to target
+    R
+        Acceleration factor for the acquisition.
+
+    Returns
+    -------
+    Simulation
+        The simulation object associated with the data.
+
+    See Also
+    --------
+    simfmri.simulator.simulation.Simulation
+        The data container for the simulation.
+    """
+    sim_data = Simulation(shape=shape, n_frames=n_frames, TR=1, n_coils=1)
+
+    simulator = (
+        SheppLoganPhantomGeneratorHandler()
+        @ ActivationHandler.from_block_design(3, 3, n_frames)
+        @ AcquisitionHandler.vds(acs=24, accel=R, constant=True, gen_smaps=False)
+    )
+
+    return simulator(sim_data)
