06bd5fddc3248f82b388846fbcdfc0271cdc27a9
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-01-03 17:02:14 +0100

    add simulation factory

--- src/simfmri/simulator/scenarios.py
+++ src/simfmri/simulator/scenarios.py
@@ -2,15 +2,49 @@
 
 from simfmri.utils import Shape2d3d
 
-from .simulation import Simulation
+from .simulation import SimulationData, SimulationParams
 from .handlers import (
+    AbstractHandler,
     SheppLoganPhantomGeneratorHandler,
     ActivationHandler,
     AcquisitionHandler,
+    KspaceNoiseHandler,
 )
 
 
-def sl_block_vds(n_frames: int, shape: Shape2d3d, snr: float, accel: int) -> Simulation:
+class SimulationDataFactory:
+    """Simulation Factory.
+
+    The Simulation data is generated throught the `simulate` function.
+
+    Parameters
+    ----------
+    sim_params
+        Essential parameters for a simulation
+    handlers
+        list of handler to apply to get the fully generated simulation data.
+
+    Notes
+    -----
+    This is best used with hydra configuration files. see `conf/simulations/`
+    """
+
+    def __init__(self, sim_params: SimulationParams, handlers: list[AbstractHandler]):
+        self.sim_params = sim_params
+        self.handlers = handlers
+
+    def simulate(self):
+        """Build the simulation data."""
+        sim = SimulationData.from_params(self.sim_params)
+
+        for handler in self.handlers:
+            sim = handler.handle(sim)
+        return sim
+
+
+def sl_block_vds(
+    n_frames: int, shape: Shape2d3d, snr: float, accel: int
+) -> SimulationData:
     """Generate a simulation scenario using shepp-logan and block activation.
 
     The TR is fixed to 1s.
@@ -36,7 +70,7 @@ def sl_block_vds(n_frames: int, shape: Shape2d3d, snr: float, accel: int) -> Sim
     simfmri.simulator.simulation.Simulation
         The data container for the simulation.
     """
-    sim_data = Simulation(shape=shape, n_frames=n_frames, TR=1, n_coils=1)
+    sim_data = SimulationData(shape=shape, n_frames=n_frames, TR=1, n_coils=1)
 
     simulator = (
         SheppLoganPhantomGeneratorHandler()
