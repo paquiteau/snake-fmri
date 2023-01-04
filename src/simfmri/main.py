147bd5285ba830763b782ff2187b8eabaf71e9df
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-01-04 16:49:21 +0100

    add simple main script

--- src/simfmri/main.py
+++ src/simfmri/main.py
@@ -1,1 +1,22 @@
 """Main script fot the reconstruction validation."""
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import numpy as np
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main_app(cfg: DictConfig) -> None:
+    """Perform simulation, reconstruction and validation of fMRI data."""
+    print(OmegaConf.to_yaml(cfg, resolve=True))
+
+    simulation_factory = hydra.utils.instantiate(cfg.simulation)
+    reconstructor = hydra.utils.instantiate(cfg.reconstruction)
+
+    sim = simulation_factory.simulate()
+    np.save("data_ref.npy", sim.data_ref)
+    print(reconstructor)
+
+
+if __name__ == "__main__":
+    main_app()
