94dfc6fd0d92b71caf7cf6b475e205e6e784a026
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-01-11 10:54:49 +0100

    gather the json files in a csv table after multirun.

--- src/simfmri/hydra/callback.py
+++ src/simfmri/hydra/callback.py
@@ -1,8 +1,9 @@
 """Callback mechanism for hydra  jobs."""
 import os
+import json
+import glob
 
 from hydra.experimental.callback import Callback
-from hydra.core.utils import JobReturn
 from omegaconf import DictConfig
 import pandas as pd
 
@@ -10,30 +11,17 @@ import pandas as pd
 class MultiRunGatherer(Callback):
     """Callback to gather job results."""
 
-    def __init__(self) -> None:
-        self.results = []
-
-    def on_job_end(
-        self, config: DictConfig, job_return: JobReturn, **kwargs: None
-    ) -> None:
-        """Run at the end of each job.
-
-        Notes
-        -----
-        Needs to be defined in hydra config at `hydra/callbacks/multirun_callback`.
-        """
-        results = {}
-        for overrider in job_return.overrides:
-            key, val = overrider.split("=")
-            results[key] = val
-        results |= job_return.return_value
-        self.results.append(results)
-
     def on_multirun_end(self, config: DictConfig, **kwargs: None) -> None:
         """Run after all job have ended.
 
         Will write a DataFrame from all the results. at the run location.
         """
-        df = pd.DataFrame(self.results)
         save_dir = config.hydra.sweep.dir
-        df.to_csv(os.path.join(save_dir, "results.csv"))
+        os.chdir(save_dir)
+        results = []
+        for filename in glob.glob("*/result.json"):
+            with open(filename) as f:
+                results.append(json.load(f))
+
+        df = pd.DataFrame(results)
+        df.to_csv("agg_results.csv")
