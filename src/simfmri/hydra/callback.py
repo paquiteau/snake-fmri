54e23fa7df10cb7f373525adc7cbad75ccc8fdf1
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-01-13 11:18:58 +0100

    add latest link callback.

--- src/simfmri/hydra/callback.py
+++ src/simfmri/hydra/callback.py
@@ -1,9 +1,11 @@
 """Callback mechanism for hydra  jobs."""
 import os
+import errno
 import json
 import glob
 
 from hydra.experimental.callback import Callback
+from hydra.utils import to_absolute_path
 from omegaconf import DictConfig
 import pandas as pd
 
@@ -25,3 +27,46 @@ class MultiRunGatherer(Callback):
 
         df = pd.DataFrame(results)
         df.to_csv("agg_results.csv")
+
+
+class LatestRunLink(Callback):
+    """Callback that create a symlink to the latest run in the base output dir.
+
+    Parameters
+    ----------
+    run_base_dir
+        name of the basedir
+    multirun_base_dir
+
+    """
+
+    def __init__(
+        self, run_base_dir: str = "outputs", multirun_base_dir: str = "multirun"
+    ):
+        self.run_base_dir = to_absolute_path(run_base_dir)
+        self.multirun_base_dir = to_absolute_path(multirun_base_dir)
+
+    def on_run_end(self, config: DictConfig, **kwargs: None) -> None:
+        """Execute after a single run."""
+        self._on_anyrun_end(config.hydra.run.dir, self.run_base_dir)
+
+    def on_multirun_end(self, config: DictConfig, **kwargs: None) -> None:
+        """Execute after a multi run."""
+        self._on_anyrun_end(config.hydra.sweep.dir, self.multirun_base_dir)
+
+    def _on_anyrun_end(self, run_dir: str, base_dir: str) -> None:
+        self._force_symlink(
+            to_absolute_path(run_dir),
+            to_absolute_path(os.path.join(base_dir, "latest")),
+        )
+
+    def _force_symlink(self, src: str, dest: str) -> None:
+        """Create a symlink from src to test, overwriting dest if necessary."""
+        try:
+            os.symlink(src, dest)
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                os.remove(dest)
+                os.symlink(src, dest)
+            else:
+                raise e
