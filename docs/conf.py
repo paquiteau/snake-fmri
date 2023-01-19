107a8c1dd72b5e04741a76043d6072404adb9dc3
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-01-19 14:42:13 +0100

    added a basic logo.

--- docs/conf.py
+++ docs/conf.py
@@ -71,6 +71,7 @@ html_theme = "pydata_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_logo = "brain_with_roi_nobg.png"
 html_context = {
     # ...
     "default_mode": "light"
