303029f33e4f07bc7c6ee7056264ad2355b75b9a
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-23 11:28:08 +0200

    update the statistical step use new module.

--- src/simfmri/runner/cli/main.py
+++ src/simfmri/runner/cli/main.py
@@ -6,9 +6,9 @@ import numpy as np
 from hydra_callbacks import PerfLogger
 from omegaconf import DictConfig, OmegaConf
 
-from simfmri.runner.glm import compute_confusion, compute_confusion_stats, compute_test
-
-from simfmri.runner.utils import dump_confusion, product_dict, save_data
+from simfmri.runner.glm import get_all_confusion
+from simfmri.runner.utils import dump_confusion, save_data
+from simfmri.runner.metric import get_snr, get_ptsnr
 
 log = logging.getLogger(__name__)
 
@@ -41,35 +41,24 @@ def main_app(cfg: DictConfig) -> None:
 
     log.debug("Current simulation state: %s", sim)
     with PerfLogger(log, name="Estimation"):
-        stat_configs = product_dict(**OmegaConf.to_container(cfg.stats))
-        contrasts = []
-        estimations = []
-        confusions = []
-        for stat_conf in stat_configs:
-            estimation, design_matrix, contrast = compute_test(
-                sim=sim,
-                data_test=np.moveaxis(data_test, 0, -1),
-                **stat_conf,
-            )
-            contrasts.append(np.squeeze(contrast))
-            estimations.append(np.squeeze(estimation))
-            confusion = compute_confusion(np.squeeze(estimation), sim.roi)
-            confusion |= stat_conf
-            confusions.append(confusion)
+        contrast, thresh_mat = get_all_confusion(data_test, sim, **cfg.stats)
+
+        metrics = {
+            "ptsnr_roi": get_ptsnr(data_test, sim.data, sim.roi),
+            "ptsnr": get_ptsnr(data_test, sim.data),
+            "snr_roi": get_snr(data_test, sim.data, sim.roi),
+            "snr": get_snr(data_test, sim.data),
+        }
     # 3. Clean up and saving
     with PerfLogger(log, name="Cleanup"):
-        sim.extra_infos["contrast"] = contrasts
-        sim.extra_infos["estimation"] = estimations
-
+        sim.extra_infos["contrast"] = contrast
+        sim.extra_infos["stats"] = thresh_mat
+        sim.extra_infos["metrics"] = metrics
         sim.extra_infos["data_test"] = np.squeeze(data_test)
 
         if cfg.save and cfg.save.data:
             save_data(cfg.save.data, cfg.save.compress, sim, log)
-        confusion_overriden = dump_confusion(confusions)
-    for c in confusion_overriden:
-        log.info(
-            c | compute_confusion_stats(c["f_neg"], c["t_neg"], c["f_pos"], c["t_pos"])
-        )
+
     log.info(PerfLogger.recap())
 
 
