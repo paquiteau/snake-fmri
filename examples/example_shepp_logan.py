1469e7641ca2fec9c7f1e5edf35330bf9a3cdd17
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-08-03 18:27:24 +0200

    update example.

--- examples/example_shepp_logan.py
+++ examples/example_shepp_logan.py
@@ -42,7 +42,7 @@ print(sim_data)
 # Then we are going to build the simulator from elementary steps,
 # which all *handles* a particular aspect of the simulation.
 #
-# The handlers can be chained easily by using the `@` operator (or by setting the
+# The handlers can be chained easily by using the `>>` operator (or by setting the
 # ``next`` attribute of an Handler)
 # Some handlers comes with preset function to ease their creation.
 #
@@ -51,11 +51,11 @@ simulator = (
     # Create a shepp logan phantom
     SheppLoganGeneratorHandler()
     # Add activations (and creates a time dimension)
-    @ ActivationHandler.from_block_design(3, 3, n_frames)
+    >> ActivationHandler.from_block_design(3, 3, n_frames)
     # simulate the acquisition
-    @ AcquisitionHandler.vds(acs=24, accel=accel, constant=True, gen_smaps=False)
+    >> AcquisitionHandler.vds(acs=24, accel=accel, constant=True, gen_smaps=False)
     # add noise to the kspace
-    @ KspaceNoiseHandler(snr=snr)
+    >> KspaceNoiseHandler(snr=snr)
 )
 
 print(simulator.get_chain())
