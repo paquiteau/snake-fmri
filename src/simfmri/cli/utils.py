e3f7fffa610ae0ef8e527d68292cc4f7cdb89a10
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-10-10 09:26:00 +0200

    remove useless column.

--- src/simfmri/cli/utils.py
+++ src/simfmri/cli/utils.py
@@ -186,8 +186,5 @@ def aggregate_results(results_files: list[str]) -> str:
     results_proc = pd.json_normalize([json.load(open(r)) for r in results_files])
     df = pd.DataFrame(results_proc)
     # Some light preprocessing.
-    df["config.reconstruction._target_"] = df["config.reconstructor._target_"].apply(
-        lambda x: x.split(".")[-1].replace("Reconstructor", "")
-    )
     df.to_parquet("results_gathered.gzip")
     return os.getcwd() + "/results_gathered.gzip"
