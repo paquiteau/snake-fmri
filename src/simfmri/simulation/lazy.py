a194e10a6e471b7f94530e184411b72e3faed156
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-11-17 09:29:44 +0100

    feat: create lazy array from existing lazy array.

--- src/simfmri/simulation/lazy.py
+++ src/simfmri/simulation/lazy.py
@@ -80,10 +80,16 @@ class LazySimArray(Sequence):
 
     """
 
-    def __init__(self, base_array: np.ndarray = None, n_frames: int = 1):
+    def __init__(
+        self,
+        base_array: np.ndarray | LazySimArray = None,
+        n_frames: int = None,
+    ):
+        self._n_frames = n_frames
+        if isinstance(base_array, LazySimArray) and n_frames is None:
+            self._n_frames = len(base_array)
         self._base_array = base_array
         self._operations: list[Callable[np.ndarray, int, ...], np.ndarray] = []
-        self._n_frames = n_frames
 
     @property
     def shape(self) -> tuple[int, ...]:
