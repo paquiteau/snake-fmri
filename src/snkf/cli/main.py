8bc292d6831608f4709e27d29a28e11a05cacd00
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-02-16 14:00:34 +0100

    fix: save the data before doing analysis.

--- src/snkf/cli/main.py
+++ src/snkf/cli/main.py
@@ -74,12 +74,13 @@ def main_app(cfg: DictConfig) -> None:
             logger.debug(f"Skipped {rec_name}, no parametrization")
             continue
         data_test, rec_str = reconstruct(sim_file, rec_name, params)
+        np.save(f"data_rec_{rec_str}.npy", data_test)
+
         logger.debug("Current simulation state: %s", sim)
         with PerfLogger(logger, name="Analysis " + str(rec_str)):
             zscore = contrast_zscore(data_test, sim, cfg.stats.contrast_name)
             stats = get_scores(zscore, sim.roi)
 
-        np.save(f"data_rec_{rec_str}.npy", data_test)
         np.save(f"data_zscore_{rec_str}.npy", zscore)
         results.append(
             {
