45db284dc95bfeb70fb80ac986957cca965713a6
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-03-29 13:03:13 +0100

    low memory setup

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,5 +4,9 @@ from hydra.test_utils.test_utils import run_python_script
 
 
 def test_scenario1():
-    cmd = ["src/snkf/cli/main.py", "--config-name=scenario1.yaml"]
+    cmd = [
+        "src/snkf/cli/main.py",
+        "--config-name=scenario1.yaml",
+        "++handlers.acquisition-vds.n_jobs=1",  # to avoid to much memory consumption.
+    ]
     result, _err = run_python_script(cmd, allow_warnings=True)
