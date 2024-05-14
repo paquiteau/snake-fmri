ab7f3937df9182f79ebe4504f86bde3eb25902e6
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-05-14 18:45:47 +0200

    cleanup the documentation

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -3,7 +3,7 @@
 rm -rf _build _api
 
 # Build the API documentation with autoapi
-sphinx-apidoc -ePf --ext-autodoc --output-dir=_api -t _templates/ ../src/snkf  "**/_version.py"
+#sphinx-apidoc -ePf --ext-autodoc --output-dir=_api -t _templates/ ../src/snkf  "**/_version.py"
 
 # Finally build the entire docs
 jupyter-book build .
