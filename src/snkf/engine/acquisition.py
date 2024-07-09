8748495ea6b2c3027099f29e6d4b43de19e34e57
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-07-09 13:46:31 +0200

    fix: correct GRE equation.

--- src/snkf/engine/acquisition.py
+++ src/snkf/engine/acquisition.py
@@ -39,14 +39,30 @@ def get_contrast_gre(
 ) -> NDArray:
     """Compute the GRE contrast at TE."""
     return (
-        np.sin(FA)
-        * np.exp(-TE / phantom.tissue_properties[:, PropTissueEnum.T2])
+        phantom.tissue_properties[:, PropTissueEnum.rho]
+        * np.sin(np.deg2rad(FA))
+        * np.exp(-TE / phantom.tissue_properties[:, PropTissueEnum.T2s])
         * (1 - np.exp(-TR / phantom.tissue_properties[:, PropTissueEnum.T1]))
         / (
             1
-            - np.cos(FA) * np.exp(-TR / phantom.tissue_properties[:, PropTissueEnum.T1])
+            - np.cos(np.deg2rad(FA))
+            * np.exp(-TR / phantom.tissue_properties[:, PropTissueEnum.T1])
         )
     )
+
+
+def get_ideal_phantom(phantom: Phantom, sim_conf: SimConfig):
+    """Apply the contrast to the phantom and return volume."""
+
+    contrast = get_contrast_gre(
+        phantom, sim_conf.seq.FA, sim_conf.seq.TE, sim_conf.seq.TR
+    )
+    print(contrast)
+    phantom_state = np.sum(
+        phantom.tissue_masks * contrast[(..., *([None] * len(phantom.anat_shape)))],
+        axis=0,
+    )
+    return phantom_state
 
 
 def iter_traj_stacked(
