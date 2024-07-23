1174a6321de02315d7e30c55319d81ab0703cb1a
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-07-23 08:54:27 +0200

    feat: add handler chain

--- src/snake/handlers/base.py
+++ src/snake/handlers/base.py
@@ -1,7 +1,11 @@
 """Base handler module."""
 
+from __future__ import annotations
+import yaml
+import dataclasses
+
 from .._meta import MetaDCRegister
-from typing import ClassVar, TypeVar
+from typing import ClassVar, TypeVar, Any
 
 from ..simulation import SimConfig
 from ..phantom import Phantom, DynamicData, KspaceDynamicData
@@ -18,6 +22,7 @@ class MetaHandler(MetaDCRegister):
 class AbstractHandler(metaclass=MetaHandler):
     """Handler Interface."""
 
+    __registry__: ClassVar[dict[str, type[AbstractHandler]]]
     __handler_name__: ClassVar[str]
 
     def get_static(self, phantom: Phantom, sim_conf: SimConfig) -> Phantom:
@@ -31,3 +36,42 @@ class AbstractHandler(metaclass=MetaHandler):
     def get_dynamic_kspace(self, sim_conf: SimConfig) -> KspaceDynamicData | None:
         """Get the dynamic kspace information of the handler."""
         return None
+
+    def to_yaml(self) -> str:
+        """Show the yaml config associated with the handler."""
+        return yaml.dump(dataclasses.asdict(self))  # type: ignore
+
+
+class HandlerChain:
+    """Represent a Chain of Handler, that needs to be apply to a simulation."""
+
+    def __init__(self, *args: AbstractHandler):
+        self._handlers = list(args)
+
+    def __eq__(self, other: Any):
+        if not isinstance(other, HandlerChain):
+            return NotImplemented
+        return self._handlers == other._handlers
+
+    def __repr__(self):
+        """Represent a simulation."""
+        ret = "Handler Chain: "
+        for h in self._handlers:
+            ret += f"{h} >> "
+        ret = ret[:-3]
+        return ret
+
+
+# short alias
+H = AbstractHandler.__registry__
+handler = H
+
+
+def list_handlers() -> list[str]:
+    """List all available handlers."""
+    return list(H.keys())
+
+
+def get_handler(name: str) -> type[AbstractHandler]:
+    """Get a handler from its name."""
+    return H[name]
