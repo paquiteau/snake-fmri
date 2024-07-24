9a093a047b9feef79744847ae7f2f3e3f9a5729c
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-07-24 09:04:52 +0200

    feat: add joint acquisition - reconstruction cli.

---
+++ src/snake_toolkit/cli/main.py
@@ -0,0 +1,29 @@
+import hydra
+from omegaconf import OmegaConf
+from snake.handlers import HandlerList, AbstractHandler
+from snake.phantom import Phantom
+from snake.simulation import SimConfig
+from snake.sampling import BaseSampler
+from snake.mrd_utils import make_base_mrd, MRDLoader
+from snake.engine import BaseAcquisitionEngine
+
+
+from .acquisition import acquisition
+from .config import ConfigSNAKE
+from .reconstruction import reconstruction
+
+
+def main(cfg: ConfigSNAKE) -> None:
+    """Do Acquisition and Reconstruction sequentially."""
+    cfg = OmegaConf.to_object(cfg)
+    print(cfg)
+    acquisition(cfg)
+    reconstruction(cfg)
+
+
+main_cli = hydra.main(
+    version_base=None, config_path="../../cli-conf/", config_name="config"
+)(main)
+
+if __name__ == "__main__":
+    main_cli()
