d5a5dff128c49951b9b3e30181c22769eb0eb714
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-07-24 09:01:46 +0200

    feat: generate smaps.

---
+++ src/snake_toolkit/cli/acquisition.py
@@ -0,0 +1,75 @@
+"""CLI for SNAKE."""
+
+import logging
+import hydra
+
+from omegaconf import OmegaConf
+from snake.engine import BaseAcquisitionEngine
+from snake.handlers import HandlerList
+from snake.mrd_utils import make_base_mrd
+from snake.phantom import Phantom
+from snake.smaps import get_smaps
+from .config import (
+    ConfigSNAKE,
+)
+
+log = logging.getLogger(__name__)
+
+
+def acquisition(cfg: ConfigSNAKE) -> None:
+    """Simulate acquisition."""
+    cfg: ConfigSNAKE = OmegaConf.to_object(cfg)
+    # FIXME: Hydra should be able to do that on its own.
+    print(cfg)
+    sim_conf = cfg.sim_conf
+    if cfg.phantom.name == "brainweb":
+        phantom = Phantom.from_brainweb(
+            sub_id=cfg.phantom.sub_id,
+            sim_conf=sim_conf,
+            tissue_select=cfg.phantom.tissue_select,
+        )
+    else:
+        raise ValueError(f"Unknown phantom {cfg.phantom.name}")
+
+    handlers = HandlerList(*cfg.handlers.values())
+
+    if len(cfg.sampler) > 1:
+        log.warning(
+            "Multiple sampler configuration detected. Only the first one is used."
+        )
+    sampler = list(cfg.sampler.values())[0]
+
+    for h in handlers:
+        phantom = h.get_static(phantom, sim_conf)
+
+    dynamic_data = [h.get_dynamic(phantom, sim_conf) for h in handlers]
+
+    smaps = None
+    if sim_conf.hardware.n_coils > 1:
+        smaps = get_smaps(sim_conf.shape, n_coils=sim_conf.hardware.n_coils)
+
+    make_base_mrd(cfg.filename, sampler, phantom, sim_conf, dynamic_data, smaps)
+    log.info("Initialization done")
+
+    engine_klass = BaseAcquisitionEngine.__registry__[sampler.__engine__]
+    kwargs = {}
+    if engine_klass.__engine_name__ == "NUFFT":
+        kwargs["nufft_backend"] = cfg.engine.nufft_backend
+    engine = engine_klass(mode=cfg.engine.mode, snr=cfg.engine.snr, **kwargs)
+
+    engine(
+        cfg.filename,
+        worker_chunk_size=cfg.engine.chunk_size,
+        n_workers=cfg.engine.n_jobs,
+    )
+
+    log.info("Acquisition done")
+    log.info("Output file is at %s", cfg.filename)
+
+
+acquisition_cli = hydra.main(
+    version_base=None, config_path="../../cli-conf", config_name="config"
+)(acquisition)
+
+if __name__ == "__main__":
+    acquisition_cli()
--- src/snake_toolkit/cli/config.py
+++ src/snake_toolkit/cli/config.py
@@ -1,11 +1,14 @@
 """Configuration of SNAKE using Hydra."""
 
 from typing import Any
-import hydra
-
 from dataclasses import dataclass, field
+from hydra.core.config_store import ConfigStore
+from omegaconf import OmegaConf
 
 from snake.simulation import SimConfig
+
+from snake.handlers import AbstractHandler
+from snake.sampling import BaseSampler
 
 
 @dataclass
@@ -23,25 +26,31 @@ class EngineConfig:
 class PhantomConfig:
     """PhantomConfig."""
 
-    name = "brainweb"
+    name: str = "brainweb"
     sub_id: int = 4
+    tissue_select: list[str] = field(default_factory=list)
 
 
 @dataclass
 class ConfigSNAKE:
     """Configuration for SNAKE."""
 
-    sim_config: SimConfig
     handlers: Any
     sampler: Any
-    engine: EngineConfig
+    sim_conf: SimConfig = SimConfig()
+    engine: EngineConfig = EngineConfig()
     phantom: PhantomConfig = PhantomConfig()
 
-    force_sim: bool = False
     cache_dir: str = "${oc.env:PWD}/cache"
     result_dir: str = "${oc.env:PWD}/results"
-    ignore_pattern: list[str] = field(default_factory=lambda: ["n_jobs"])
-    filename: str = "scenario1.mrd"
+    ignore_patterns: list[str] = field(default_factory=lambda: ["n_jobs"])
+    filename: str = "test.mrd"
+
+
+# Custom Resolver for OmegaConf
+# allows to do:
+#     _target_: {$snake.handler:motion-image}
+# instead of hardcoding the path to the class
 
 
 def snake_handler_resolver(name: str) -> str:
@@ -58,3 +67,17 @@ def snake_sampler_resolver(name: str) -> str:
 
     cls = BaseSampler.__registry__[name]
     return cls.__module__ + "." + cls.__name__
+
+
+OmegaConf.register_new_resolver("snake.handler", snake_handler_resolver)
+OmegaConf.register_new_resolver("snake.sampler", snake_sampler_resolver)
+
+
+cs = ConfigStore.instance()
+cs.store(name="base_config", node=ConfigSNAKE)
+
+for handler_name, cls in AbstractHandler.__registry__.items():
+    cs.store(group="handlers", name=handler_name, node={handler_name: cls})
+
+for sampler, cls in BaseSampler.__registry__.items():
+    cs.store(group="sampler", name=sampler, node={sampler: cls})
