51502b0ba17d2159e66507acada06b9381b09621
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-07-30 12:52:17 +0200

    perf: engine nufft is faster.

--- src/snake/engine/nufft.py
+++ src/snake/engine/nufft.py
@@ -19,7 +19,6 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
     """Acquisition engine using nufft."""
 
     __engine_name__ = "NUFFT"
-    nufft_backend: str = "finufft"
     mode: str = "simple"
     snr: float = np.inf
 
@@ -42,7 +41,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
         head = dataset._dataset["data"][0]["head"]
         n_samples = head["number_of_samples"]
         ndim = head["trajectory_dimensions"]
-        trajectories = np.zeros(len(shot_idx), n_samples, ndim)
+        trajectories = np.zeros((len(shot_idx), n_samples, ndim), dtype=np.float32)
 
         for i, s in enumerate(shot_idx):
             trajectories[i] = dataset._dataset["data"][s]["traj"].reshape(
@@ -55,7 +54,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
         samples: NDArray, sim_conf: SimConfig, smaps: NDArray, backend: str
     ) -> FourierOperatorBase:
         """Initialize the nufft operator."""
-        n_coils = len(smaps) if smaps else 1
+        n_coils = len(smaps) if smaps is not None else 1
         kwargs = {}
         if "stacked" in backend:
             kwargs["z_index"] = "auto"
@@ -77,8 +76,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
         sim_conf: SimConfig,
         trajectories: NDArray,
         smaps: NDArray,
-        backend: str,
-        echo_sample_idx: int,
+        nufft_backend: str,
     ) -> np.ndarray:
         """Acquire k-space data with T2s relaxation effect."""
         chunk_size, n_samples, ndim = trajectories.shape
@@ -88,7 +86,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
         )
         # (n_tissues_true, n_samples) Filter the tissues that have NaN Values.
         nufft = NufftAcquisitionEngine._init_model_nufft(
-            trajectories[0], sim_conf, smaps, backend=backend
+            trajectories[0], sim_conf, smaps, backend=nufft_backend
         )
         echo_idx = np.argmin(np.sum(np.abs(trajectories[0]) ** 2), axis=-1)
 
@@ -107,8 +105,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
                 sim_conf.seq.TR,
             )
             phantom_state = (
-                1000
-                * contrast[(..., *([None] * len(frame_phantom.anat_shape)))]
+                contrast[(..., *([None] * len(frame_phantom.anat_shape)))]
                 * frame_phantom.masks
             )
             nufft.samples = traj
@@ -126,8 +123,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
         sim_conf: SimConfig,
         trajectories: NDArray,
         smaps: NDArray,
-        backend: str,
-        echo_sample_idx: int,
+        nufft_backend: str,
     ) -> np.ndarray:
         """Acquire k-space data. No T2s decay."""
         chunk_size, n_samples, ndim = trajectories.shape
@@ -136,7 +132,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
             (chunk_size, sim_conf.hardware.n_coils, n_samples), dtype=np.complex64
         )
         nufft = NufftAcquisitionEngine._init_model_nufft(
-            trajectories[0], sim_conf, smaps, backend=backend
+            trajectories[0], sim_conf, smaps, backend=nufft_backend
         )
         # (n_tissues_true, n_samples) Filter the tissues that have NaN Values
         for i, traj in enumerate(trajectories):
@@ -151,8 +147,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
                 sim_conf.seq.TR,
             )
             phantom_state = np.sum(
-                1000
-                * contrast[(..., *([None] * len(phantom.anat_shape)))]
+                contrast[(..., *([None] * len(phantom.anat_shape)))]
                 * frame_phantom.masks,
                 axis=0,
             )
@@ -163,7 +158,7 @@ class NufftAcquisitionEngine(BaseAcquisitionEngine):
     def _write_chunk_data(
         self, dataset: mrd.Dataset, chunk: Sequence[int], chunk_data: NDArray
     ) -> None:
-        for i, shot in enumerate(chunk):
-            acq = dataset.read_acquisition(shot)
-            acq.data[:] = chunk_data[i]
-            dataset.write_acquisition(acq, shot)
+        shot_idx = np.asarray(chunk)
+        acq_chunk = dataset._dataset["data"][shot_idx]
+        acq_chunk["data"] = chunk_data.view(np.float32).reshape(*acq_chunk.shape, -1)
+        dataset._dataset["data"][shot_idx] = acq_chunk
