b75dab6dc1ef0430a6a9619cd608e7b66cc93449
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-10-16 09:30:02 +0200

    fix: black

--- src/snake/core/handlers/activations/__init__.py
+++ src/snake/core/handlers/activations/__init__.py
@@ -1,4 +1,5 @@
 """Activation handler module."""
+
 from .activations import BlockActivationHandler
 from .bold import get_bold
 from .roi import BRAINWEB_OCCIPITAL_ROI, get_indices_inside_ellipsoid
