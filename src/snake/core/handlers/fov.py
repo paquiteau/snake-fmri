fix: when n_coils=1 no smaps computation is required.

src/snake/core/handlers/fov.py
@@ -131,13 +131,6 @@ class FOVHandler(AbstractHandler):
             ),
             dtype=phantom.masks.dtype,
         )
-        new_smaps = np.zeros(
-            (
-                phantom.smaps.shape[0],
-                *tuple(round(size_vox[i] / zoom_factor[i]) for i in range(3)),
-            ),
-            dtype=phantom.smaps.dtype,
-        )
 
         run_parallel(
             _apply_transform,
@@ -149,17 +142,27 @@ class FOVHandler(AbstractHandler):
             angles=self.angles,
             zoom_factor=zoom_factor,
         )
-
-        run_parallel(
-            _apply_transform,
-            phantom.smaps,
-            new_smaps,
-            parallel_axis=0,
-            center=center_vox,
-            size=size_vox,
-            angles=self.angles,
-            zoom_factor=zoom_factor,
-        )
+        if phantom.smaps is not None:
+            return Phantom.from_masks(new_masks, sim_conf)
+            new_smaps = np.zeros(
+                (
+                    phantom.smaps.shape[0],
+                    *tuple(round(size_vox[i] / zoom_factor[i]) for i in range(3)),
+                ),
+                dtype=phantom.smaps.dtype,
+            )
+            run_parallel(
+                _apply_transform,
+                phantom.smaps,
+                new_smaps,
+                parallel_axis=0,
+                center=center_vox,
+                size=size_vox,
+                angles=self.angles,
+                zoom_factor=zoom_factor,
+            )
+        else:
+            new_smaps = None
 
         # Create a new phantom with updated masks
         new_phantom = phantom.copy()

src/snake/core/phantom/static.py
@@ -151,15 +151,19 @@ class Phantom:
         )
         tissues_mask = tissue_resized
 
+        smaps = None
+        if sim_conf.hardware.n_coils > 1:
+            smaps = get_smaps(
+                tissues_mask.shape[1:],
+                n_coils=sim_conf.hardware.n_coils,
+            )
+
         return cls(
             "brainweb",
             tissues_mask,
             labels=np.array([t[0] for t in tissues_list]),
             props=np.array([t[1:] for t in tissues_list]),
-            smaps=get_smaps(
-                tissues_mask.shape[1:],
-                n_coils=sim_conf.hardware.n_coils,
-            ),
+            smaps=smaps,
         )
 
     @classmethod
@@ -202,7 +206,7 @@ class Phantom:
         meta_sr = mrd.Meta(
             {
                 "name": self.name,
-                "labels": f'{",".join(self.labels)}',
+                "labels": f"{','.join(self.labels)}",
                 "props": serialize_array(self.props),
             }
         ).serialize()
@@ -251,7 +255,9 @@ class Phantom:
         with array_from_shm(mask_prop, label_prop, properties_prop, smaps_prop) as arrs:
             yield cls(name, *arrs)
 
-    def in_shared_memory(self, manager: SharedMemoryManager) -> tuple[
+    def in_shared_memory(
+        self, manager: SharedMemoryManager
+    ) -> tuple[
         tuple[str, ArrayProps, ArrayProps, ArrayProps, ArrayProps | None],
         tuple[SharedMemory, SharedMemory, SharedMemory, SharedMemory | None],
     ]: