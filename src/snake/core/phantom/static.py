a24ad44f3d3aa309812264f1fb1b5e4f266ac2a4
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2025-02-17 23:23:21 +0100

    feat: phantom now also store information about affine matrice

--- src/snake/core/phantom/static.py
+++ src/snake/core/phantom/static.py
@@ -8,21 +8,26 @@ import contextlib
 import logging
 import os
 from collections.abc import Generator
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import IntEnum
 from importlib.resources import files
 from multiprocessing.managers import SharedMemoryManager
 from multiprocessing.shared_memory import SharedMemory
-from typing import TypeVar, Any
+from typing import Literal, TypeVar, Any, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from _typeshed import GenericPath
 
 import ismrmrd as mrd
 import numpy as np
 from numpy.typing import NDArray
+from nibabel.nifti1 import Nifti1Image
 
-from ..parallel import ArrayProps, array_from_shm, array_to_shm, run_parallel
-from snake._meta import NoCaseEnum
+from ..parallel import ArrayProps, array_from_shm, array_to_shm
+from snake._meta import NoCaseEnum, ThreeFloats
 from ..simulation import SimConfig
-
+from snake.core.parallel import run_parallel
+from snake.core.phantom.utils import resize_tissues
 from snake.core.smaps import get_smaps
 
 log = logging.getLogger(__name__)
@@ -54,6 +59,9 @@ class Phantom:
     labels: NDArray[np.string_]
     props: NDArray[np.float32]
     smaps: NDArray[np.complex64] | None = None
+    affine: NDArray[np.float32] = field(
+        default_factory=lambda: np.eye(4, dtype=np.float32)
+    )
 
     def add_tissue(
         self,
@@ -99,22 +107,42 @@ class Phantom:
         tissue_file: str | TissueFile = TissueFile.tissue_1T5,
         tissue_select: list[str] | None = None,
         tissue_ignore: list[str] | None = None,
+        output_res: float | ThreeFloats = 0.5,
     ) -> Phantom:
-        """Get the Brainweb Phantom."""
-        from brainweb_dl import BrainWebTissuesV2, get_mri
+        """Get the Brainweb Phantom.
 
-        from .utils import resize_tissues
+        Parameters
+        ----------
+        sub_id: int
+            Subject ID of the brainweb dataset.
+        sim_conf: SimConfig
+            Simulation configuration.
+        tissue_file: str
+            File with the tissue properties.
+        tissue_select: list[str]
+            List of tissues to select.
+        tissue_ignore: list[str]
+            List of tissues to ignore.
+        output_res: float
+            Resolution of the output phantom.
+
+        Returns
+        -------
+        Phantom
+            The phantom object.
+        """
+        from brainweb_dl import BrainWebTissuesV2, get_mri
 
         if tissue_ignore and tissue_select:
             raise ValueError("Only one of tissue_select or tissue_ignore can be used.")
+        if tissue_select:
+            tissue_select = [t.lower() for t in tissue_select]
+        if tissue_ignore:
+            tissue_ignore = [t.lower() for t in tissue_ignore]
 
-        # TODO: Use the sim shape properly.
-        tissues_mask = get_mri(sub_id, contrast="fuzzy").astype(np.float32)
-        z = (
-            np.array([0.5, 0.5, 0.5])
-            * np.array(sim_conf.shape)
-            / np.array(sim_conf.fov_mm)
-        )
+        tissues_mask, affine = get_mri(sub_id, contrast="fuzzy", with_affine=True)
+        tissues_mask = tissues_mask.astype(np.float32)
+        affine = affine.astype(np.float32)
         tissues_mask = np.ascontiguousarray(tissues_mask.T)
         tissues_list = []
         try:
@@ -146,18 +174,27 @@ class Phantom:
         log.info(
             f"Selected tissues: {select}, {[t[0] for t in tissues_list]}",
         )
+        if len(tissues_list) == 0:
+            raise ValueError("No tissues selected")
         tissues_mask = tissues_mask[select]
         shape = tissues_mask.shape
-        new_shape = (shape[0], *np.round(np.array(shape[1:]) * z).astype(int))
-        tissue_resized = np.zeros(new_shape, dtype=np.float32)
-        run_parallel(
-            resize_tissues,
-            tissues_mask,
-            tissue_resized,
-            parallel_axis=0,
-            z=tuple(z),
-        )
-        tissues_mask = tissue_resized
+        #
+        # TODO: Use the sim shape properly.
+        if output_res != 0.5:
+            if isinstance(output_res, float):
+                output_res = [output_res] * 3
+            z = np.array([0.5, 0.5, 0.5]) / np.array(output_res)
+            new_shape = (shape[0], *np.round(np.array(shape[1:]) * z).astype(int))
+            tissue_resized = np.zeros(new_shape, dtype=np.float32)
+
+            run_parallel(
+                resize_tissues,
+                tissues_mask,
+                tissue_resized,
+                parallel_axis=0,
+                z=tuple(z),
+            )
+            tissues_mask = tissue_resized
 
         smaps = None
         if sim_conf.hardware.n_coils > 1:
@@ -172,17 +209,8 @@ class Phantom:
             labels=np.array([t[0] for t in tissues_list]),
             props=np.array([t[1:] for t in tissues_list]),
             smaps=smaps,
+            affine=affine,
         )
-
-    @classmethod
-    def from_shepp_logan(cls, resolution: tuple[int]) -> Phantom:
-        """Get the Shepp-Logan Phantom."""
-        raise NotImplementedError
-
-    @classmethod
-    def from_guerin_kern(cls, resolution: tuple[int]) -> Phantom:
-        """Get the Guerin-Kern Phantom."""
-        raise NotImplementedError
 
     @classmethod
     def from_mrd_dataset(
@@ -196,16 +224,47 @@ class Phantom:
         labels = np.array(image.meta["labels"].split(","))
         props = unserialize_array(image.meta["props"])
 
+        # Affine matrix from the header
+        position = image._head.position
+        read_dir = image._head.read_dir
+        phase_dir = image._head.phase_dir
+        slice_dir = image._head.slice_dir
+        affine = np.eye(4, dtype=np.float32)
+        res = np.array(image._head.field_of_view) / np.array(image._head.matrix_size)
+        print("res", tuple(image._head.field_of_view))
+        affine[:3, 3] = -position[0], -position[1], position[2]
+        affine[:3, 0] = (
+            -read_dir[0] * res[0],
+            -read_dir[1] * res[0],
+            read_dir[2] * res[0],
+        )
+        affine[:3, 1] = (
+            -phase_dir[0] * res[1],
+            -phase_dir[1] * res[1],
+            phase_dir[2] * res[1],
+        )
+        affine[:3, 2] = (
+            -slice_dir[0] * res[2],
+            -slice_dir[1] * res[2],
+            slice_dir[2] * res[2],
+        )
+
+        # smaps
+        try:
+            smaps = dataset.read_image("smaps", imnum).data
+        except LookupError:
+            smaps = None
+
         return cls(
             masks=image.data,
             labels=labels,
             props=props,
             name=name,
+            affine=affine,
+            smaps=smaps,
         )
 
-    def to_mrd_dataset(
-        self, dataset: mrd.Dataset, sim_conf: SimConfig, imnum: int = 0
-    ) -> mrd.Dataset:
+    def to_mrd_dataset(self, dataset: mrd.Dataset | GenericPath) -> mrd.Dataset:
         """Add the phantom as an image to the dataset."""
         # Create the image
         if not isinstance(dataset, mrd.Dataset):
@@ -216,16 +275,33 @@ class Phantom:
                 "name": self.name,
                 "labels": f"{','.join(self.labels)}",
                 "props": serialize_array(self.props),
+                "affine": serialize_array(self.affine),
             }
         ).serialize()
+
+        # Convert the affine matrix to position, field of view, etc.
+        offsets = self.affine[:3, 3]
+        position = (-offsets[0], -offsets[1], offsets[2])
+        read_dir = self.affine[:3, 0] / self.affine[0, 0]
+        read_dir = (-read_dir[0], -read_dir[1], read_dir[2])
+        phase_dir = self.affine[:3, 1] / self.affine[1, 1]
+        phase_dir = (-phase_dir[0], -phase_dir[1], phase_dir[2])
+        slice_dir = self.affine[:3, 2] / self.affine[2, 2]
+        slice_dir = (-slice_dir[0], -slice_dir[1], slice_dir[2])
+
+        fov_mm = tuple(np.array(self.anat_shape) * np.diag(self.affine)[:3])
 
         # Add the phantom data
         dataset.append_image(
             "phantom",
             mrd.image.Image(
                 head=mrd.image.ImageHeader(
-                    matrixSize=mrd.xsd.matrixSizeType(*self.anat_shape),
-                    fieldOfView_mm=mrd.xsd.fieldOfViewMm(*sim_conf.fov_mm),
+                    matrix_size=self.anat_shape,
+                    field_of_view=fov_mm,
+                    position=position,
+                    phase_dir=phase_dir,
+                    slice_dir=slice_dir,
+                    read_dir=read_dir,
                     channels=self.n_tissues,
                     acquisition_time_stamp=0,
                     attribute_string_len=len(meta_sr),
@@ -240,8 +316,12 @@ class Phantom:
                 "smaps",
                 mrd.image.Image(
                     head=mrd.image.ImageHeader(
-                        matrixSize=mrd.xsd.matrixSizeType(*self.smaps.shape[1:]),
-                        fieldOfView_mm=mrd.xsd.fieldOfViewMm(*sim_conf.fov_mm),
+                        matrix_size=self.anat_shape,
+                        field_of_view=fov_mm,
+                        position=position,
+                        phase_dir=phase_dir,
+                        slice_dir=slice_dir,
+                        read_dir=read_dir,
                         channels=len(self.smaps),
                         acquisition_time_stamp=0,
                     ),
@@ -259,29 +339,110 @@ class Phantom:
         properties_prop: ArrayProps,
         label_prop: ArrayProps,
         smaps_prop: ArrayProps,
+        affine_prop: ArrayProps,
     ) -> Generator[Phantom, None, None]:
         """Give access the tissue masks and properties in shared memory."""
-        with array_from_shm(mask_prop, label_prop, properties_prop, smaps_prop) as arrs:
+        with array_from_shm(
+            mask_prop, label_prop, properties_prop, smaps_prop, affine_prop
+        ) as arrs:
             yield cls(name, *arrs)
 
     def in_shared_memory(
         self, manager: SharedMemoryManager
     ) -> tuple[
-        tuple[str, ArrayProps, ArrayProps, ArrayProps, ArrayProps | None],
-        tuple[SharedMemory, SharedMemory, SharedMemory, SharedMemory | None],
+        tuple[str, ArrayProps, ArrayProps, ArrayProps, ArrayProps | None, ArrayProps],
+        tuple[
+            SharedMemory, SharedMemory, SharedMemory, SharedMemory | None, SharedMemory
+        ],
     ]:
         """Add a copy of the phantom in shared memory."""
         tissue_mask, _, tisue_mask_smm = array_to_shm(self.masks, manager)
         tissue_props, _, tissue_prop_smm = array_to_shm(self.props, manager)
         labels, _, labels_sm = array_to_shm(self.labels, manager)
+        affine, _, affine_sm = array_to_shm(self.affine, manager)
         if self.smaps is not None:
             smaps, _, smaps_sm = array_to_shm(self.smaps, manager)
         else:
             smaps, smaps_sm = None, None
 
         return (
-            (self.name, tissue_mask, tissue_props, labels, smaps),
-            (tisue_mask_smm, tissue_prop_smm, labels_sm, smaps_sm),
+            (self.name, tissue_mask, tissue_props, labels, smaps, affine),
+            (tisue_mask_smm, tissue_prop_smm, labels_sm, smaps_sm, affine_sm),
+        )
+
+    def masks2nifti(self) -> Nifti1Image:
+        """Return the masks of the phantom as a Nifti object."""
+        return Nifti1Image(
+            self.masks,
+            affine=self.affine,
+            extra={"props": self.props, "labels": self.labels},
+        )
+
+    def smaps2nifti(self) -> Nifti1Image:
+        """Return the smaps as a Nifti object."""
+        if self.smaps:
+            return Nifti1Image(self.smaps, affine=self.affine)
+        else:
+            raise ValueError("No Smaps to convert.")
+
+    def contrast(
+        self,
+        *,
+        TR: float | None = None,
+        TE: float | None = None,
+        FA: float | None = None,
+        sequence: Literal["GRE"] = "GRE",
+        sim_conf: SimConfig | None = None,
+        aggregate: bool = True,
+    ) -> NDArray[np.float32]:
+        """Compute the contrast of the phantom for a given sequence.
+
+        Parameters
+        ----------
+        TR: float
+        TE: float
+        FA: float
+        sim_conf: SimConfig
+            Other way to provide sequence parameters
+        aggregate: bool, optional default=True
+            Sum all the tissues contrast for getting a single image.
+        sequence="GRE"
+            Default value, no other value is currently supported.
+
+        Results
+        -------
+        NDArray
+            The constrast of the tissues.
+        """
+        if sim_conf is not None:
+            TR = sim_conf.seq.TR
+            TE = sim_conf.seq.TE
+            FA = sim_conf.seq.FA
+        if sim_conf is None and TR is None and TE is None and FA is None:
+            raise ValueError("Missing either sim_conf or TR,TE,FA")
+        if sequence.upper() == "GRE":
+            contrasts = self._contrast_gre(TR=TR, TE=TE, FA=FA)
+        else:
+            raise NotImplementedError("Contrast not implemented.")
+        if aggregate:
+            ret = np.zeros(self.anat_shape, dtype=np.float32)
+            for c, m in zip(contrasts, self.masks, strict=False):
+                ret += c * m
+            return ret
+        else:
+            return self.masks * contrasts[(..., *([None] * len(self.anat_shape)))]
+
+    def _contrast_gre(self, *, TR: float, TE: float, FA: float) -> NDArray:
+        return (
+            self.props[:, PropTissueEnum.rho]
+            * np.sin(np.deg2rad(FA))
+            * np.exp(-TE / self.props[:, PropTissueEnum.T2s])
+            * (1 - np.exp(-TR / self.props[:, PropTissueEnum.T1]))
+            / (
+                1
+                - np.cos(np.deg2rad(FA))
+                * np.exp(-TR / self.props[:, PropTissueEnum.T1])
+            )
         )
 
     @property
---
+++ tests/test_phantom.py
@@ -0,0 +1,78 @@
+"""Test on the phantom object."""
+
+from pathlib import Path
+from multiprocessing.managers import SharedMemoryManager
+from pytest_cases import parametrize_with_cases, parametrize, fixture
+import numpy as np
+from snake.core import Phantom, SimConfig
+
+
+@fixture(scope="module")
+def sim_config() -> SimConfig:
+    return SimConfig(
+        max_sim_time=10,
+    )
+
+
+class CasesPhantom:
+    """Test cases for the Phantom object."""
+
+    @parametrize(n_coils=(1, 8))
+    def case_mini_phantom(self, n_coils: int) -> Phantom:
+        affine = np.eye(4)
+        affine[:3, :] = np.random.rand(3, 4)
+        phantom = Phantom(
+            "dummy",
+            masks=np.random.uniform(0, 1, (5, 10, 10, 10)),
+            labels=np.array(["A", "B", "C", "D", "E"]),
+            props=np.random.rand(5, 5),
+            smaps=None if n_coils == 1 else np.random.rand(n_coils, 10, 10, 10),
+            affine=affine,
+        )
+        return phantom
+
+
+@parametrize_with_cases(
+    "phantom",
+    cases=CasesPhantom,
+)
+def test_multiprocessing(phantom: Phantom):
+    """Check that the shared memory serialization works."""
+    with SharedMemoryManager() as smm:
+        array_props, _ = phantom.in_shared_memory(smm)
+        with Phantom.from_shared_memory(*array_props) as phantom2:
+            assert phantom.name == phantom2.name
+            assert np.all(phantom.masks == phantom2.masks)
+            assert np.all(phantom.labels == phantom2.labels)
+            assert np.all(phantom.props == phantom2.props)
+            assert np.all(phantom.smaps == phantom2.smaps)
+            assert np.all(phantom.affine == phantom2.affine)
+
+
+@parametrize_with_cases(
+    "phantom",
+    cases=CasesPhantom,
+)
+def test_mrd(phantom: Phantom, tmpdir: Path):
+    """Test that the phantom is correctly written and red back"""
+
+    dataset = tmpdir / "phantom.mrd"
+
+    phantom.to_mrd_dataset(dataset)
+    phantom2 = Phantom.from_mrd_dataset(dataset)
+    print(phantom2.affine)
+    print(phantom.affine)
+    assert phantom.name == phantom2.name
+    assert np.all(phantom.masks == phantom2.masks)
+    assert np.all(phantom.labels == phantom2.labels)
+    assert np.all(phantom.props == phantom2.props)
+    assert np.all(phantom.smaps == phantom2.smaps)
+    assert np.allclose(phantom.affine, phantom2.affine)
+
+
+@parametrize_with_cases("phantom", cases=CasesPhantom)
+def test_contrast(phantom, sim_config):
+    """Test that the phantom can be used in a simulation."""
+    contrast = phantom.contrast(sim_config=sim_config)
+    # FIXME: This is not the correct way to test the contrast
+    assert contrast.shape == sim_config.fov.shape
