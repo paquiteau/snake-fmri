05cec998e1165ce6bf4faa34c50f1144b15f4444
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2025-02-21 16:41:44 +0100

    refactor!: Remove FOVHandler

--- src/snake/core/handlers/__init__.py
+++ src/snake/core/handlers/__init__.py
@@ -4,14 +4,12 @@ from .base import AbstractHandler, HandlerList, get_handler, H
 from .activations import BlockActivationHandler
 from .noise import NoiseHandler
 from .motion import RandomMotionImageHandler
-from .fov import FOVHandler
 
 __all__ = [
     "AbstractHandler",
     "HandlerList",
     "get_handler",
     "H",
-    "FOVHandler",
     "BlockActivationHandler",
     "NoiseHandler",
     "RandomMotionImageHandler",
